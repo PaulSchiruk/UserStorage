b6c0aa3bfaa0f12145b3bc4fcc2e3837b2c58ffc
PaulSchiruk <paul.schiruk@gmail.com>
2017-11-14 17:41:24 +0300

    Replace Console.WriteLine methods with Trace. Add XML listeners. Fix stylecop issues.

--- UserStorage/UserStorageServices/UserStorageServiceLog.cs
+++ UserStorage/UserStorageServices/UserStorageServiceLog.cs
@@ -11,15 +11,17 @@ namespace UserStorageServices
     {
         private readonly BooleanSwitch logging = new BooleanSwitch("enableLogging", "switch in app.config");
 
-        public UserStorageServiceLog(IUserStorageService storageService) : base(storageService) { }
+        public UserStorageServiceLog(IUserStorageService storageService) : base(storageService)
+        {
+        }
 
         public override int Count
         {
             get
             {
-                if (logging.Enabled)
+                if (this.logging.Enabled)
                 {
-                    Console.WriteLine("Count() method is called.");
+                    Trace.WriteLine("Count() method is called.");
                 }
 
                 return StorageService.Count;
@@ -28,81 +30,89 @@ namespace UserStorageServices
 
         public override void Add(User user)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("Add() method is called.");
+                Trace.WriteLine("Add() method is called.");
             }
 
             StorageService.Add(user);
         }
+
         public override bool Remove(User user)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("Remove() method is called.");
+                Trace.WriteLine("Remove() method is called.");
             }
 
             return StorageService.Remove(user);
         }
+
         public override IEnumerable<User> SearchByFirstName(string firstName)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByFirstName() method is called.");
+                Trace.WriteLine("SearchByFirstName() method is called.");
             }
 
             return StorageService.SearchByFirstName(firstName);
         }
+
         public override IEnumerable<User> SearchByFirstNameAndLastName(string firstName, string lastName)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByFirstNameAndLastName() method is called.");
+                Trace.WriteLine("SearchByFirstNameAndLastName() method is called.");
             }
 
             return StorageService.SearchByFirstNameAndLastName(firstName, lastName);
         }
+
         public override IEnumerable<User> SearchByFirstNameAndAge(string firstName, int age)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByFirstNameAndAge() method is called.");
+                Trace.WriteLine("SearchByFirstNameAndAge() method is called.");
             }
 
             return StorageService.SearchByFirstNameAndAge(firstName, age);
         }
+
         public override IEnumerable<User> SearchByLastName(string lastName)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByLastName() method is called.");
+                Trace.WriteLine("SearchByLastName() method is called.");
             }
 
             return StorageService.SearchByLastName(lastName);
         }
+
         public override IEnumerable<User> SearchByLastNameAndAge(string lastName, int age)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByLastNameAndAge() method is called.");
+                Trace.WriteLine("SearchByLastNameAndAge() method is called.");
             }
 
             return StorageService.SearchByLastNameAndAge(lastName, age);
         }
+
         public override IEnumerable<User> SearchByAge(int age)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByAge() method is called.");
+                Trace.WriteLine("SearchByAge() method is called.");
             }
 
             return StorageService.SearchByAge(age);
         }
+
         public override IEnumerable<User> SearchByFirstNameAndLastNameAndAge(string firstName, string lastName, int age)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByFirstNameAndLastNameAndAge() method is called.");
+                Trace.WriteLine("SearchByFirstNameAndLastNameAndAge() method is called.");
             }
 
             return StorageService.SearchByFirstNameAndLastNameAndAge(firstName, lastName, age);
@@ -110,9 +120,9 @@ namespace UserStorageServices
 
         public override IEnumerable<User> Search(Predicate<User> comparer)
         {
-            if (logging.Enabled)
+            if (this.logging.Enabled)
             {
-                Console.WriteLine("SearchByFirstNameAndLastNameAndAge() method is called.");
+                Trace.WriteLine("SearchByFirstNameAndLastNameAndAge() method is called.");
             }
 
             return StorageService.Search(comparer);
